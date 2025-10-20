0169de32a6cbc5645b3ed6ec331969c8546031a2
tchicherin <tchicherin.alt@gmail.com>
2025-10-20 18:16:54 +0300

    removed some pornography from Midsho

--- src/Utils.h
+++ src/Utils.h
@@ -1,18 +1,26 @@
 #pragma once
+
+#ifndef UTILS_H
+#define UTILS_H
+
 #include <random>
 
 struct Rng {
-    static std::mt19937& gen() {
-        static std::random_device rd;
-        static std::mt19937 g(rd());
-        return g;
-    }
-    static double uniform(double a, double b) {
-        std::uniform_real_distribution<double> d(a, b);
-        return d(gen());
-    }
-    static double normal(double mean, double stddev) {
-        std::normal_distribution<double> d(mean, stddev);
-        return d(gen());
-    }
+  static std::mt19937& gen() {
+    static std::random_device rd;
+    static std::mt19937 g(rd());
+    return g;
+  }
+
+  static double uniform(double a, double b) {
+    std::uniform_real_distribution<double> d(a, b);
+    return d(gen());
+  }
+
+  static double normal(double mean, double stddev) {
+    std::normal_distribution<double> d(mean, stddev);
+    return d(gen());
+  }
 };
+
+#endif // UTILS_H
